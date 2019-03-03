ff731c542d959d7c5494a383f9df3d6c4f450bde
jayeshdalal <jayeshdalal7@gmail.com>
2019-03-03 12:36:19 +0530

    add HTML test Runner

---
+++ com/webDriver/DemoHTMLReport.py
@@ -0,0 +1,47 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common import keys
+from webdriver_manager.chrome import ChromeDriverManager
+import HtmlTestRunner
+
+
+
+
+class MyTestCase(unittest.TestCase):
+    # Class level setup
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+    def test_openOrangeUrl(self):
+        url = self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_xpath("//input[@id='txtUsername']").send_keys("Admin")
+        self.driver.find_element_by_xpath("//input[@id='txtPassword']").send_keys("admin123")
+        self.driver.find_element_by_xpath("//input[@id='btnLogin']").click()
+        title = self.driver.title
+        print (title)
+        print(self.driver.current_url)
+
+        self.assertEqual(title, "OrangeHRM")
+
+    def test_openGoogle(self):
+        url = self.driver.get("https://google.com")
+        self.driver.find_element_by_name("q").send_keys("automation step by step")
+        # keys.keys (Enter )
+        self.driver.implicitly_wait(2)
+        self.driver.find_element_by_name("q").send_keys(keys.Keys.ESCAPE)
+        self.driver.find_element_by_name("btnK").click()
+        self.driver.implicitly_wait(5)
+        title = self.driver.title
+        self.assertEqual(title, "automation step by step - Google Search")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
