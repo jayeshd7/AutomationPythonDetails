3ee6c2b38368b5d34004664dd53d50481f16d89c
jayeshdalal <jayeshdalal7@gmail.com>
2019-03-04 09:56:35 +0530

    Pytest demo

---
+++ com/PyTestDemo/test_PyTestDemo.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+class PyTestDemo():
+    @pytest.fixture()
+    def test_setup(self):
+            global driver
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+            driver.implicitly_wait(10)
+            driver.maximize_window()
+            yield
+            driver.close()
+            driver.quit()
+
+    def test_openOrangeUrl(self, test_setup):
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        driver.find_element_by_xpath("//input[@id='txtUsername']").send_keys("Admin")
+        driver.find_element_by_xpath("//input[@id='txtPassword']").send_keys("admin123")
+        # Explicit wait
+        driver.find_element_by_xpath("//input[@id='btnLogin']").click()
+        title = driver.title
+        assert title == "OrangeHRM"
+    '''
+    @pytest.yield_fixture()
+    def test_tearDown():
+        driver.close()
+        driver.quit()
+    '''
+
+
+
+
+
